ccefcd0abb2041a7bc6bbf70329acad38dbc8d99
Julius <julius.tumas.contractor@draeger.com>
2023-05-19 15:59:54 +0300

    minor

--- LPV_IC/azure_devops_validation_tests/azure_devops_validation_tests/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AzureDevOpsValidationTests;
-
-public static class Constants
-{
-    public const string AppSettingsFileName = "appSettings.test.json";
-    public const string AppSettingsSectionName = "AppSettings";
-    public const string PatchOperationFieldPathPrefix = "/fields/";
-}
